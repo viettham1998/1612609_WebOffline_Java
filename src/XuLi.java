1082c7255d96004d1c53144ed81ba32939453745
tham <viettham1998@gmail.com>
2019-05-23 12:05:06 +0700

    lọc link trùng, tải ảnh, css...

--- src/XuLi.java
+++ src/XuLi.java
@@ -1,3 +1,5 @@
+import jdk.nashorn.internal.scripts.JO;
+
 import javax.swing.*;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -13,8 +15,9 @@ public class XuLi {
     String chuoi;
     String tenFile = "index.html";
     String tenThuMuc = "1612609";
-    String taoThuMuc="";
+    String taoThuMuc = "";
     ArrayList<String> arr = new ArrayList<String>();
+    ArrayList<String> dsLink = new ArrayList<>();
 
     public XuLi() {
         chuoi = "";
@@ -59,6 +62,7 @@ public class XuLi {
                 FileOutputStream fos = new FileOutputStream(tenThuMuc + "\\" + tenFile);
                 fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
                 fos.close();
+                dsLink.add(chuoi);
                 docTatCaDuongDan(chuoi);
             } catch (IOException ex) {
                 ex.printStackTrace();
@@ -73,13 +77,9 @@ public class XuLi {
         try {
             URL website = new URL(a);
             ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-            //đọc a, tính//
-            //Nếu còn // thì tạo thư mục tên là .... new File(tenthumuc).mkdir()
-            //tiếp tục đọc, nếu hết // thì tạo file.
-            FileOutputStream fos = new FileOutputStream(tenThuMuc + "\\"+ taoThuMuc + "\\" + name);
+            FileOutputStream fos = new FileOutputStream(tenThuMuc + "\\" + taoThuMuc + "\\" + name);
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
             fos.close();
-            JOptionPane.showMessageDialog(null, "tao thanh cong");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -97,25 +97,29 @@ public class XuLi {
                 int to = line.lastIndexOf("\"");
                 arr.add(line.substring(from + 1, to));
                 String M = duongDanChuan(line.substring(from + 1, to));
-                //JOptionPane.showMessageDialog(null, M);
-                String N = M;
-                if(M.endsWith("css")==true || M.endsWith("ico")==true){
-                    t = M.lastIndexOf("/");
-                    tenFileLuu = N.substring(t + 1);
-                    JOptionPane.showMessageDialog(null, tenFileLuu);
-                    int k = M.indexOf("/",9);
-                    taoThuMuc = N.substring(k + 1, t);
-                    new File(tenThuMuc + "\\" + taoThuMuc).mkdirs();
+                if (kiemTraLinkNgoai(duongDan, M) == false) {
+                    if(laLinkTrung(M,dsLink)==false) {
+                        t = M.lastIndexOf("/");
+                        if (M.endsWith("/") == false) {
+                            tenFileLuu = M.substring(t + 1);
+                            if (tenFileLuu.contains(".") == false) {
+                                tenFileLuu = tenFileLuu + ".html";
+                            }
+                            int k = M.indexOf("/", 9);
+                            if (k != t) {
+                                taoThuMuc = M.substring(k + 1, t);
+                            }
+                            new File(tenThuMuc + "\\" + taoThuMuc).mkdirs();
+
+                        } else {
+                            tenFileLuu = "home" + i + ".html";
+                            i++;
+                        }
+                        Load(M, tenFileLuu);
+                        taoThuMuc = "";
+                    }
+                    dsLink.add(M);
                 }
-                else
-                {
-                    tenFileLuu = "output" + i + ".html";
-                    i++;
-                }
-                //đọc từ cuối, xác định lưu css hay html
-                //lấy tên
-                Load(M, tenFileLuu);
-                taoThuMuc="";
             }
         }
         FileOutputStream dsLink = new FileOutputStream(tenThuMuc + "\\" + "dsLink.txt");
@@ -139,15 +143,52 @@ public class XuLi {
         }
         if (dem == 2)
             duongDan = duongDan + '/';
+        // JOptionPane.showMessageDialog(null, duongDan);
         return duongDan;
     }
 
     public String duongDanChuan(String duongDan) {
-        String k = duongDan.substring(0, 4);
-        if (k.compareTo("http") != 0) {
+        if (duongDan.startsWith("http") == false) {
             duongDan = chuanHoaDuongDan(chuoi) + duongDan;
+//        }
         }
         return duongDan;
     }
 
+    public Boolean laLinkTrung(String link, ArrayList<String> dsLink) {
+        for (String chay : dsLink) {
+            if (link.equals(chay)) {
+                dsLink.remove(link);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String catDuongDan(String duongDanCat) {
+        String tmp = duongDanCat;
+        int k = tmp.indexOf("/", 9);
+        if (tmp.startsWith("https://") == true) {
+            tmp = tmp.substring(8, k - 1);
+            if (tmp.startsWith("www.") == true) {
+                tmp = tmp.substring(4);
+            }
+        } else if (tmp.startsWith("http://") == true) {
+            tmp = tmp.substring(7, k - 1);
+            if (tmp.startsWith("www") == true) {
+                tmp = tmp.substring(4);
+            }
+        }
+        return tmp;
+    }
+
+    public boolean kiemTraLinkNgoai(String duongDanBanDau, String duongDan) {
+        String tmp = duongDan;
+        if(tmp.substring(tmp.lastIndexOf("/")).contains(".")==true)
+            //JOptionPane.showMessageDialog(null,"Kiem tra: " + tmp);
+            return false;
+        if (catDuongDan(duongDanBanDau).equals(catDuongDan(duongDan)) == true)
+            return false;
+        return true;
+    }
 }
